dd01dc92f10c629d73d5d5f96c3240b6fc66280f
ferdyankml <ferdyankml@gmail.com>
2026-05-10 11:06:48 +0700

    menu selesai

--- index.php
+++ index.php
@@ -1,11 +1,118 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <h1>tes</h1>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">PesanKuy</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pesanan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Status</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- navbar end -->
+
+
+    <!-- content -->
+    <main>
+        <h1 class="sub">Menu</h1>
+        <div class="row row-cols-3 row-cols-md-5 g-4">
+            <div class="col">
+                <div class="card">
+                    <img src="asset/img_FuVZ92o.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Latte Coffe</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <a href="#" class="btn btn-primary">Tambahkan</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="asset/img_FuVZ92o.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Latte Coffe</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <a href="#" class="btn btn-primary">Tambahkan</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="asset/img_FuVZ92o.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Latte Coffe</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <a href="#" class="btn btn-primary">Tambahkan</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">  
+                    <img src="asset/img_FuVZ92o.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Latte Coffe</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <a href="#" class="btn btn-primary">Tambahkan</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">  
+                    <img src="asset/img_FuVZ92o.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Latte Coffe</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <a href="#" class="btn btn-primary">Tambahkan</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">  
+                    <img src="asset/img_FuVZ92o.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Latte Coffe</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <a href="#" class="btn btn-primary">Tambahkan</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!-- content end-->
+
+
+    <!-- footer -->
+    <footer>
+        <div class="fot">
+            <p>Pesanan Menjadi lebih Mudah, PesanKuy@2026</p>
+        </div>
+    </footer>
+    <!-- footer end-->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
